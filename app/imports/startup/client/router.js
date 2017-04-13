bccd9833b34417952deb2933197d401ba6e8513c
Kenji Clemmer <clemmer@hawaii.edu>
2017-04-12 19:55:56 -1000

    Routing

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -76,6 +76,14 @@ userRoutes.route('/filter', {
   },
 });
 
+export const sellPageRouteName = 'Sell_Page';
+userRoutes.route('/sell', {
+  name: sellPageRouteName,
+  action() {
+    BlazeLayout.render('User_Layout', { main: sellPageRouteName });
+  },
+});
+
 export const faqPageRouteName = 'FAQ_Page';
 userRoutes.route('/faq', {
   name: faqPageRouteName,
