47a3c0cf20321975c82108958ab9461c0ce7d517
Kenji Clemmer <clemmer@hawaii.edu>
2017-05-09 22:55:08 -1000

    Eslint Errors

--- app/imports/ui/pages/mybooks/mybooks-page.js
+++ app/imports/ui/pages/mybooks/mybooks-page.js
@@ -1,6 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
-import { _ } from 'meteor/underscore';
 import { Books } from '/imports/api/book/BookCollection.js';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
@@ -15,7 +14,7 @@ Template.MyBooks_Page.helpers({
    * @returns {*} User's Book documents.
    */
   booksFilter() {
-    return Books.find({username: FlowRouter.getParam('username')});
+    return Books.find({ username: FlowRouter.getParam('username')} );
   },
   routeUserName() {
     return FlowRouter.getParam('username');
